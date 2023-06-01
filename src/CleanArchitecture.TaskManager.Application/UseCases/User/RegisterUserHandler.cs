4a53025f94399e0104b15e8fad6c0e0745b53060
Eduardo Leonardo <eduardo.rosa@terceiroslinx.com.br>
2023-06-01 17:20:12 -0300

    Commands and handlers for useCases

---
+++ src/CleanArchitecture.TaskManager.Application/UseCases/User/RegisterUserHandler.cs
@@ -0,0 +1,18 @@
+﻿using CleanArchitecture.TaskManager.Application.Commands;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.TaskManager.Application.UseCases.User
+{
+    public class RegisterUserHandler : IRequestHandler<RegisterUserCommand, bool>
+    {
+        public Task<bool> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- src/CleanArchitecture.TaskManager.Common/Communication/Mediator/Publisher.cs
+++ src/CleanArchitecture.TaskManager.Common/Communication/Mediator/Publisher.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,21 @@ namespace CleanArchitecture.TaskManager.Common.Communication.Mediator
 {
     public class Publisher
     {
+        private readonly IMediator _mediator;
+
+        public Publisher(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task Publish(EventMessage message)
+        {
+            await _mediator.Publish(message);
+        }
+
+        public async Task SendCommand(CommandMessage message)
+        {
+            await _mediator.Send(message);
+        }
     }
 }
